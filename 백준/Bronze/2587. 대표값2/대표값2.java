7d9adb01c520a0dc2b5e9a0f15998d3f3a2d1029
judith <141399892+jud1thDev@users.noreply.github.com>
2024-07-14 23:55:41 +0900

    [Bronze II] Title: 대표값2, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.java
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -1,23 +1,33 @@
-import java.util.*;
+import java.io.*;
+import java.util.Arrays;
 
-public class Main  {
-    static ArrayList<Integer> cards = new ArrayList<>();
-
-    public static void main(String [] args){
-    
-        Scanner scanner = new Scanner(System.in);
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int[] num = new int[5];
         for (int i = 0; i < 5; i++) {
-            int input = scanner.nextInt();
-            // 유효성
-            if(input % 10 == 0 && input < 100){num[i] = input;}
-                else return;
+            int input = Integer.parseInt(br.readLine());
+            // 유효성 검사
+            if (input % 10 == 0 && input < 100) {
+                num[i] = input;
+            } else {
+                return;
+            }
         }
 
         Arrays.sort(num); // 오름차순 정렬
-        System.out.println(Arrays.stream(num).sum()/5); // 평균값
-        System.out.print(num[2]); //  중앙값
-    }
+        int average = Arrays.stream(num).sum() / 5; // 평균값
+        int median = num[2]; // 중앙값
 
+        bw.write(String.valueOf(average));
+        bw.newLine();
+        bw.write(String.valueOf(median));
+        bw.newLine();
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
