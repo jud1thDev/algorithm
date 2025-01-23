0d98eeadad4c83ec266d709cff3fd662fd30ea0a
judith <8w8u8@naver.com>
2025-01-23 22:45:01 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,8 @@
+score = int(input())
+
+# 파이썬에 switch 문법 없음
+if score >= 90: print ('A')
+elif score >= 80: print ('B')
+elif score >= 70: print ('C')
+elif score >= 60: print ('D')
+else: print('F')
