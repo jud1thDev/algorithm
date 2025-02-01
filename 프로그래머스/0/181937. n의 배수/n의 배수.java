d402746ff8dc176f5e9571785cb97d29ba9b7dd8
judith <8w8u8@naver.com>
2025-02-01 23:36:40 +0900

    [level 0] Title: n의 배수, Time: 0.03 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/0/181937. n의 배수/n의 배수.java
+++ 프로그래머스/0/181937. n의 배수/n의 배수.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(int num, int n) {
-        if (num % n == 0) return n = 1;
-        else return n = 0;
+        if (num % n == 0) return 1;
+        else return 0;
     }
 }
