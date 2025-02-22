c0e4a1790ae8536f3a273e6d4d8477567e8c9bea
judith <8w8u8@naver.com>
2025-02-22 23:28:51 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 3.22 ms, Memory: 92.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] ans = Arrays.copyOf(arr, arr.length);
+        for (int[] query : queries){
+            int s = query[0];
+            int e = query[1];
+            int k = query[2];
+            
+            for (int i = s; i <= e; i++){
+            if (i % k == 0) ans[i] += 1;
+            }
+        }
+        return ans;
+    }
+}
