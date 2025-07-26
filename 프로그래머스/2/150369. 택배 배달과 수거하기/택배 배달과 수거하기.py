b26b1ad71b715e47306812662f1bc1e50e307e1d
정유정 <8w8u8@naver.com>
2025-07-26 21:43:37 +0900

    Update 택배 배달과 수거하기.py

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,3 +1,22 @@
 def solution(cap, n, deliveries, pickups):
-    answer = -1
+    answer = 0
+    deliver_load = 0  # 누적 배달 물량
+    pickup_load = 0   # 누적 수거 물량
+    furthest_pos = n - 1  # 현재 왕복 시 가장 먼 집 위치
+
+    for i in range(n - 1, -1, -1):
+        deliver_load += deliveries[i]
+        pickup_load += pickups[i]
+
+        # 누적된 물량이 트럭 용량 초과하면 왕복 처리
+        while deliver_load > cap or pickup_load > cap:
+            answer += 2 * (furthest_pos + 1)  # 왕복 거리 누적
+            deliver_load -= cap
+            pickup_load -= cap
+            furthest_pos = i  # 가장 먼 집 위치 갱신
+
+    # 남은 물량 있으면 마지막 왕복 거리 더함
+    if deliver_load > 0 or pickup_load > 0:
+        answer += 2 * (furthest_pos + 1)
+
     return answer
