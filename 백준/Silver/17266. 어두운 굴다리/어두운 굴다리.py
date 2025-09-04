44e0ed6d0d87a25cdbddd1e5180f0ff99c06b7ae
정유정 <8w8u8@naver.com>
2025-09-04 09:53:44 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 88 ms, Memory: 42660 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
@@ -1,30 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-# 구하는 것: 굴다리 길이 N을 모두 비추기 위한 가로등의 최소 높이
-
 N = int(input())
 M = int(input())
 streetlamps = list(map(int, input().split()))
+streetlamps.sort()
 
-def binary_search(start, end, answer):
-    if start > end: 
-        return answer
-    mid = (start + end) // 2
+# 첫 구간
+max_dist = streetlamps[0]
 
-    current = 0
-    possible = True
-    for lamp in streetlamps:
-        if lamp - mid > current:
-            possible = False
-            break
-        current = max(current, lamp + mid)
-    if current < N:
-        possible = False
-    if possible:
-        return binary_search(start, mid -1, mid)
-    else:
-        return binary_search(mid + 1, end, answer)
-    
-result = binary_search(0, N, N)
-print(result)
+# 중간 구간들
+for i in range(1, M):
+    dist = (streetlamps[i] - streetlamps[i - 1]) // 2
+    if (streetlamps[i] - streetlamps[i - 1]) % 2:
+        dist += 1
+    max_dist = max(max_dist, dist)
+
+# 마지막 구간
+max_dist = max(max_dist, N - streetlamps[-1])
+
+print(max_dist)
