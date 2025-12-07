92a1ae3c727954fbd78c0d1fefd72497c71c4e56
정유정 <8w8u8@naver.com>
2025-12-07 23:56:59 +0900

    [Bronze III] Title: 학점계산, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2754. 학점계산/학점계산.py
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -3,28 +3,12 @@ input = sys.stdin.readline
 
 grade = input().strip()
 
-alphabet = grade[0]
+table = {
+    "A+": 4.3, "A0": 4.0, "A-": 3.7,
+    "B+": 3.3, "B0": 3.0, "B-": 2.7,
+    "C+": 2.3, "C0": 2.0, "C-": 1.7,
+    "D+": 1.3, "D0": 1.0, "D-": 0.7,
+    "F": 0.0
+}
 
-score = 0.0 # F
-if grade == 'F':
-    print(score)
-    exit()
-
-is_plus = grade[1]
-
-if alphabet == 'A':
-    score = 4.0
-elif alphabet == 'B':
-    score = 3.0
-elif alphabet == 'C':
-    score = 2.0
-elif alphabet == 'D':
-    score = 1.0
-
-if is_plus == '+':
-    score += 0.3
-elif is_plus == '-':
-    score -= 0.3
-
-print(score)
-       
+print(table[grade])
